e0636b3cdab076f97d3157c59d9315d1bb1366dd
Jason Vendryes <jason.vendryes@returnpath.com>
2016-12-09 15:23:03 -0700

    Cleaned up install script
    Change-Id: I586d706f2f4fe0904d44976aa47f6a53984bbf30

--- install.sh
+++ install.sh
@@ -1,51 +1,77 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Script configuration
+# Exit if a command fails
+set -o errexit
+# Exit if script tries to use undeclared variables
+set -o nounset
+# Exit status of the last command that threw a non-zero exit code
+set -o pipefail
 
 # Set default values
+timestamp=$(date +%s) # Epoch time
+re='^[1-9][0-9]*$' # Valid install options cannot start with 0
 vimrc_file=".vimrc"
-
-# Stop executing if any command in the pipe returns a non-0 status
-set -e
+bashrc_file=".bashrc"
 
 # Prompt for install options
 read -p "Available install options:
 
+Config Files
     1) .vimrc
+    2) .bashrc
+
    99) All
 
 Which do you want to install: " install_option
 
 # Check for valid install options
-re='^[1-9][0-9]*$' # Install option cannot start with 0
-if ! [[ $install_option =~ $re ]] ; then
-    echo "$vimrc_file | Error: Not a valid option!" >&2; exit 1 # User supplied unexpected input
+if ! [[ ${install_option} =~ ${re} ]] ; then
+    echo "${vimrc_file} | Error: Not a valid option!" >&2; exit 1 # User supplied unexpected input
 fi
 
-# Install .vimrc
-if [ $install_option == 1 ] ; then
-    # Is this a regular file?
-    if [ -f ~/$vimrc_file ] ; then
-        # File already exists and is a regular file
-        echo "$vimrc_file | $vimrc_file already exists as a regular file, moving on..."
-        # Move existing file to a backup file
-        if [ -L ~/$vimrc_file ] ; then
-            # File already exists and is a symbolic link
-            echo "$vimrc_file | $vimrc_file already exists as a symbolic link, exiting..." >&2; exit 1
+# Create a backup of a supplied file
+function createBackup() {
+    file=$1
+    echo "${file} already exists, creating a backup..."
+    mv -f ~/${file} ~/${file}-${timestamp}.backup
+    echo "Backup file (${file}-${timestamp}.backup) created..."
+}
+
+# Create a symbolic link
+function createSymLink() {
+    file=$1
+    echo "Creating symbolic link for ${file}..."
+    ln -s ${PWD}/${file} ~/${file}
+}
+
+# Install selected config
+function installConfig() {
+    file=$1
+    # Does the file already exist?
+    if [ -f ~/${file} ] ; then
+        echo "${file} already exists as a regular file..."
+        # File already exists and is a symbolic link, nothing more to do
+        if [ -L ~/${file} ] ; then
+            echo "${file} already exists as a symbolic link, exiting..." >&2; exit 1
         else
-            echo "$vimrc_file | $vimrc_file does NOT exist as a symbolic link, creating a backup..."
-            # Create a backup of the file
-            mv -f ~/$vimrc_file ~/$vimrc_file.backup
-            echo "$vimrc_file | Created the backup (~/$vimrc_file.backup), moving on..."
+            # Create a backup of the existing file
+            createBackup ${file}
         fi
     else
-        # This is not a regular file
-        echo "$vimrc_file | $vimrc_file does NOT exist as a regular file..."
+        # The file does not already exist
+        echo "${file} does not currently exist, proceeding with the install..."
     fi
+}
 
-    # Create symbolic link?
-    echo "$vimrc_file | Creating symbolic link for $vimrc_file..."
-    ln -s $PWD/$vimrc_file ~/$vimrc_file
+# Install .vimrc
+if [ ${install_option} == 1 ] ; then
+
+    installConfig ${vimrc_file}
+
+    createSymLink ${vimrc_file}
 
     # Install completed
-    echo "$vimrc_file | Install complete!"
+    echo "${vimrc_file} | Install complete!"
 fi
 
