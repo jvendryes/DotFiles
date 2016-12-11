fb5cc49eefd18b7a3580663515e2d37bfc66b010
Jason Vendryes <jason.vendryes@returnpath.com>
2016-12-11 10:38:53 -0700

    Code cleanup, error checking, generally more streamlined
    Change-Id: I1699de859d4e264d855c1b1b0567b8376837783b

--- install.sh
+++ install.sh
@@ -8,70 +8,96 @@ set -o nounset
 # Exit status of the last command that threw a non-zero exit code
 set -o pipefail
 
+# Config files
+vimrc=".vimrc"
+bashrc=".bashrc"
+tmux_conf=".tmux.conf"
+
 # Set default values
 timestamp=$(date +%s) # Epoch time
 re='^[1-9][0-9]*$' # Valid install options cannot start with 0
-vimrc_file=".vimrc"
-bashrc_file=".bashrc"
 
 # Prompt for install options
 read -p "Available install options:
 
 Config Files
-    1) .vimrc
-    2) .bashrc
+    1) ${vimrc}
+    2) ${bashrc}
+    3) ${tmux_conf}
 
    99) All
 
-Which do you want to install: " install_option
+What do you want to install: " install_option
 
-# Check for valid install options
-if ! [[ ${install_option} =~ ${re} ]] ; then
-    echo "${vimrc_file} | Error: Not a valid option!" >&2; exit 1 # User supplied unexpected input
-fi
-
-# Create a backup of a supplied file
+# Create a backup of selected config
 function createBackup() {
     file=$1
+    backup_file="${file}-${timestamp}.bak"
+
+    # Create backup
     echo "${file} already exists, creating a backup..."
-    mv -f ~/${file} ~/${file}-${timestamp}.backup
-    echo "Backup file (${file}-${timestamp}.backup) created..."
+    mv -f ~/${file} ~/${backup_file}
+
+    # Check for backup
+    if ! [ -f ~/${backup_file} ] ; then
+        echo "Error: Backup file was not created, exiting..." >&2; exit 1
+    else
+        echo "Backup file (~/${backup_file}) successfully created..."
+    fi
 }
 
-# Create a symbolic link
-function createSymLink() {
-    file=$1
-    echo "Creating symbolic link for ${file}..."
-    ln -s ${PWD}/${file} ~/${file}
-}
-
-# Install selected config
+# Installs the selected config
 function installConfig() {
     file=$1
+
+    # Create symlink
+    echo "Creating symlink for ${file}..."
+    ln -s ${PWD}/${file} ~/${file}
+
+    # Check for symlink
+    if ! [ -L ~/${file} ] ; then
+        echo "Error: Symlink was not created, exiting..." >&2; exit 1
+    else
+        echo "Symlink (~/${file}) successfully created..."
+    fi
+}
+
+# Checks to see if the selected config already exists
+function checkConfig() {
+    file=$1
+
     # Does the file already exist?
     if [ -f ~/${file} ] ; then
         echo "${file} already exists as a regular file..."
-        # File already exists and is a symbolic link, nothing more to do
+        # File already exists and is a symlink, nothing more to do
         if [ -L ~/${file} ] ; then
-            echo "${file} already exists as a symbolic link, exiting..." >&2; exit 1
+            echo "${file} already exists as a symlink, exiting..." >&2; exit 1
         else
             # Create a backup of the existing file
             createBackup ${file}
         fi
     else
         # The file does not already exist
-        echo "${file} does not currently exist, proceeding with the install..."
+        echo "${file} does not currently exist, installing..."
     fi
 }
 
-# Install .vimrc
-if [ ${install_option} == 1 ] ; then
+# What do you want to install?
+if ! [[ ${install_option} =~ ${re} ]] ; then
+    echo "Error: Not a valid option..." >&2; exit 1 # User supplied unexpected input
+else
+    case ${install_option} in
+        1) install_file=${vimrc} ;;
+    #    2) install_file=${bashrc} ;;
+    #    3) install_file=${tmux_config} ;;
+        *) echo "Error: Invalid selection, exiting..." >&2; exit 1
+    esac
+fi
 
-    installConfig ${vimrc_file}
-
-    createSymLink ${vimrc_file}
-
-    # Install completed
-    echo "${vimrc_file} | Install complete!"
+# Install selected config
+if [[ ${install_file} ]] ; then
+    checkConfig ${install_file}
+    installConfig ${install_file}
+    echo "Installation of ${install_file} completed successfully!"
 fi
 
