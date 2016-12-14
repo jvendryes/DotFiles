e076181d02000088276aeb6feba1c1eb56f4ecfe
jvendryes <jason@chimplabs.com>
2016-12-14 06:09:18 -0700

    Added Vundle install to vim_install.sh

--- scripts/vim_install.sh
+++ scripts/vim_install.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+# Install Vundle
+vundle_dir="${HOME}/.vim/bundle/Vundle.vim"
+if [ ! -d "${vundle_dir}" ] ; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ${vundle_dir}
+fi
+
 # Install plugins
 vim +PluginInstall +qall
